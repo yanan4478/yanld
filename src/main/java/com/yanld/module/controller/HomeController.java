675383c4c7b05967f13db093b9ddbb9fad243a74
yanan <452291096@qq.com>
2016-06-27 17:52:08 +0800

    commit

--- src/main/java/com/yanld/module/controller/HomeController.java
+++ src/main/java/com/yanld/module/controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.yanld.module.controller;
 
 import com.yanld.module.service.HomeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.inject.Inject;
+import javax.annotation.Resource;
 import java.util.Map;
 
 /**
@@ -13,16 +16,21 @@ import java.util.Map;
 @Controller
 public class HomeController {
     public static final int DEFAULT_DATE_PER_PAGE = 20;
-    private HomeService homeService = new HomeService();
 
-//    @Inject
+    @Resource
+    private HomeService homeService;
+
 //    public HomeController(HomeService homeService) {
 //        this.homeService = homeService;
 //    }
 
     @RequestMapping({"/","/home"})
     public String showHomePage(Map<String, Object> model) {
-        model.put("doudous", homeService.getDoudous(DEFAULT_DATE_PER_PAGE));
+        //ApplicationContext context = new ClassPathXmlApplicationContext("/spring/yanld.xml");
+        //HomeService homeService = (HomeService)context.getBean("homeService");
+        //homeService.print();
+//        model.put("doudous", homeService.getDoudous(DEFAULT_DATE_PER_PAGE));
+        homeService.print();
         model.put("doudou", "sqk");
         return "home";
     }
