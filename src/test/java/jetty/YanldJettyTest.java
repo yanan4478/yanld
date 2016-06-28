ade9b3101fda6552ca91bf69f8d0f4d5b9c4d0cc
yanan <452291096@qq.com>
2016-06-29 02:02:57 +0800

    commit

--- src/test/java/jetty/YanldJettyTest.java
+++ src/test/java/jetty/YanldJettyTest.java
@@ -3,12 +3,17 @@ package jetty;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 
+import java.util.Map;
+import java.util.Properties;
+
 /**
  * Created by yanan on 16/6/22.
  */
 public class YanldJettyTest {
 
     public static void doStart() {
+        System.setProperty("log4j.configuration", "file:/Users/yanan/gitHub/yanld/src/main/resources/config/log4j.properties");
+
         WebAppContext webAppContext = new WebAppContext();
         webAppContext.setContextPath("/");
         webAppContext.setDescriptor("./webapp/WEB-INF/web.xml");
